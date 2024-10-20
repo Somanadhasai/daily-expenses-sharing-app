730823d65d408eb0244fb22c17d74840bed51657
somanadha sai <149456872+Somanadhasai@users.noreply.github.com>
2024-10-20 15:28:52 +0530

    index.js

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ app.use(bodyParser.json());
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes'); 
+const expenseRoutes = require('./routes/expenseRoutes'); 
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api/users', userRoutes);
+app.use('/api/expenses', expenseRoutes);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
